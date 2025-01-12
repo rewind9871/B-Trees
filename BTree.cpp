ad1c4d1ff7f79426337188ea885ab542001267b2
rewind9871 <rewind987origin@gmail.com>
2025-01-12 17:21:39 -0500

    Initial Commit

---
+++ BTree.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+class BTree {
+    public:
+    BTree() {
+        std::cout << "Created BTree";
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "BTree.cpp"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello World!" << endl;
+    BTree tree;
+    return 0;
+}
